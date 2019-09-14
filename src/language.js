0fdef587d113d17a659da5d4f7f356ae89a78df6
Frank Navarro <fnavarro7777@gmail.com>
2019-09-14 13:20:22 -0700

    Added Spanish

--- src/language.js
+++ src/language.js
@@ -1,21 +1,25 @@
-export default ['english', 'dutch'];
+export default ['english', 'dutch', 'spanish'];
 
 export const Flags = {
   english: 'us',
   dutch: 'nl',
+  spanish: 'es',
 };
 
 export const Submit = {
   english: 'Submit',
   dutch: 'Voorleggen',
+  spanish: 'Enviar',
 };
 
 export const Name = {
   english: 'Name',
   dutch: 'Naam',
+  spanish: 'Nombre',
 };
 
 export const SelectLang = {
   english: 'Select a language',
   dutch: 'Selecteer een taal',
+  spanish: 'Selecciona un idioma',
 };

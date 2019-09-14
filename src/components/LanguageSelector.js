977eefe3a863294a48a9e7b9cf0fb7c909537f01
Frank Navarro <fnavarro7777@gmail.com>
2019-09-14 13:28:55 -0700

    Fixed flags not being clickable

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -20,7 +20,7 @@ class LanguageSelector extends Component {
 
   render() {
     return (
-      <div style={{ float: 'right' }}>
+      <div style={{ textAlign: 'right' }}>
         {SelectLang[this.context.language]}:{this.renderFlags()}
       </div>
     );
